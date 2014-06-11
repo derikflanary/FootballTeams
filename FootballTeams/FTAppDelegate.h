2ab64412bcb289888446f11f277dd9238813ca11
Joshua Howland <development@jkhowland.com>
2014-06-11 14:37:08 -0600

    Initial Commit

---
+++ FootballTeams/FTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FTAppDelegate.h
+//  FootballTeams
+//
+//  Created by Joshua Howland on 6/11/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FootballTeams/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FootballTeams
+//
+//  Created by Joshua Howland on 6/11/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FTAppDelegate class]));
+    }
+}
